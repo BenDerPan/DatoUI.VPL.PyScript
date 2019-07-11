eb37deba962ed5c76820059126a923c9b1ef68d5
BenDerPan <BenDerPan@163.com>
2019-07-11 18:09:14 +0800

    Init

---
+++ src/DatoUI.VPL.PyScript.Demo/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using DatoUI.VPL.Core;
+using System.Reflection;
+
+namespace DatoUI.VPL.PyScript.Demo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitVPL();
+        }
+
+        void InitVPL()
+        {
+            KeyDown += vplControl.VplControl_KeyDown;
+            KeyUp += vplControl.VplControl_KeyUp;
+
+           
+            var externalNodes = DatoUI.VPL.Utilities.Utilities.GetTypesInNamespace(Assembly.LoadFile(Path.Combine(Environment.CurrentDirectory, "DatoUI.VPL.PyScript.dll")), "DatoUI.VPL.PyScript.Nodes")
+                   .ToList();
+            while (true)
+            {
+                var allOk = true;
+                for (var i = 0; i < externalNodes.Count; i++)
+                {
+                    var nodeType = externalNodes[i];
+                    if (!nodeType.IsSubclassOf(typeof(Node)))
+                    {
+                        externalNodes.Remove(nodeType);
+                        allOk = false;
+                    }
+                }
+                if (allOk)
+                {
+                    break;
+                }
+            }
+
+            
+
+            vplControl.ExternalNodeTypes.AddRange(externalNodes);
+
+            vplControl.NodeTypeMode = NodeTypeModes.OnlyExternalTypes;
+
+        }
+    }
+}
---
+++ src/DatoUI.VPL.PyScript/Nodes/PyScriptNode.cs
@@ -0,0 +1,39 @@
+﻿using DatoUI.VPL.Core;
+using DatoUI.VPL.PyScript.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatoUI.VPL.PyScript.Nodes
+{
+    public class PyScriptNode : Node
+    {
+        PyScriptControl pyScript;
+        public PyScriptNode(VplControl vplControl) : base(vplControl)
+        {
+            AddInputPortToNode("Input", typeof(object));
+
+            AddOutputPortToNode("Output", typeof(object));
+            pyScript = new PyScriptControl() { Width=350,Height=200};
+            AddControlToNode(pyScript);
+            BottomComment.Text = $"Description:{Environment.NewLine}Native CPython3 script execution embed in WPF, support native python packages.";
+        }
+        public override void Calculate(object userState = null)
+        {
+            
+        }
+
+        public override Node Clone()
+        {
+            var newNode = new PyScriptNode(HostCanvas)
+            {
+                Top = Top,
+                Left = Left,
+            };
+            newNode.BottomComment.Text = BottomComment.Text;
+            return newNode;
+        }
+    }
+}
